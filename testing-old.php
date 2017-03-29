63aed03d8439fa5b283ee035aff787d81895c73d
John Bussey <johnb@svl.com>
2017-03-29 13:15:00 -0500

    init commit

---
+++ testing-old.php
@@ -0,0 +1,739 @@
+<link href="./css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="./css/internal.css" rel="stylesheet" type="text/css" />
+
+
+<div class="row">
+    <div class="col-sm-5"></div>
+    <div class="col-sm-2">
+        
+        
+        
+<div class='row chat-msg-tinselshow'>
+<div class="col-sm-6">
+tinselshow
+</div>
+<div class="col-sm-6">
+#6666ff
+</div>
+</div>
+<div class='row chat-msg-Ashioxander'>
+<div class="col-sm-6">
+Ashioxander
+</div>
+<div class="col-sm-6">
+#C069E8
+</div>
+</div>
+<div class='row chat-msg-OldWillyPete'>
+<div class="col-sm-6">
+OldWillyPete
+</div>
+<div class="col-sm-6">
+#f06cea
+</div>
+</div>
+<div class='row chat-msg-pplinwoodsnoises'>
+<div class="col-sm-6">
+pplinwoodsnoises
+</div>
+<div class="col-sm-6">
+#5f74f0
+</div>
+</div>
+<div class='row chat-msg-torque'>
+<div class="col-sm-6">
+torque
+</div>
+<div class="col-sm-6">
+#f279bc
+</div>
+</div>
+<div class='row chat-msg-imjustlo'>
+<div class="col-sm-6">
+imjustlo
+</div>
+<div class="col-sm-6">
+#ff7d98
+</div>
+</div>
+<div class='row chat-msg-Daelf'>
+<div class="col-sm-6">
+Daelf
+</div>
+<div class="col-sm-6">
+#dd7dff
+</div>
+</div>
+<div class='row chat-msg-Beewo'>
+<div class="col-sm-6">
+Beewo
+</div>
+<div class="col-sm-6">
+#ff7d7d
+</div>
+</div>
+<div class='row chat-msg-sleepyheathen'>
+<div class="col-sm-6">
+sleepyheathen
+</div>
+<div class="col-sm-6">
+#9c7dff
+</div>
+</div>
+<div class='row chat-msg-Scrapdragon'>
+<div class="col-sm-6">
+Scrapdragon
+</div>
+<div class="col-sm-6">
+#008080 
+</div>
+</div>
+<div class='row chat-msg-christinchen'>
+<div class="col-sm-6">
+christinchen
+</div>
+<div class="col-sm-6">
+#0083d2
+</div>
+</div>
+<div class='row chat-msg-riverofwhispers'>
+<div class="col-sm-6">
+riverofwhispers
+</div>
+<div class="col-sm-6">
+#8085ff
+</div>
+</div>
+<div class='row chat-msg-NoxGold'>
+<div class="col-sm-6">
+NoxGold
+</div>
+<div class="col-sm-6">
+#BB8800
+</div>
+</div>
+<div class='row chat-msg-TeapotDragon'>
+<div class="col-sm-6">
+TeapotDragon
+</div>
+<div class="col-sm-6">
+#b089fd
+</div>
+</div>
+<div class='row chat-msg-Ahzuri'>
+<div class="col-sm-6">
+Ahzuri
+</div>
+<div class="col-sm-6">
+#008B8B
+</div>
+</div>
+<div class='row chat-msg-Quirky2'>
+<div class="col-sm-6">
+Quirky2
+</div>
+<div class="col-sm-6">
+#488cff
+</div>
+</div>
+<div class='row chat-msg-WolfsHeadWolfsHeart'>
+<div class="col-sm-6">
+WolfsHeadWolfsHeart
+</div>
+<div class="col-sm-6">
+#d38df8
+</div>
+</div>
+<div class='row chat-msg-angelicxi'>
+<div class="col-sm-6">
+angelicxi
+</div>
+<div class="col-sm-6">
+#c092f0
+</div>
+</div>
+<div class='row chat-msg-Glittercritter'>
+<div class="col-sm-6">
+Glittercritter
+</div>
+<div class="col-sm-6">
+#6d96ee
+</div>
+</div>
+<div class='row chat-msg-Eter'>
+<div class="col-sm-6">
+Eter
+</div>
+<div class="col-sm-6">
+#b4975a
+</div>
+</div>
+<div class='row chat-msg-prismhues'>
+<div class="col-sm-6">
+prismhues
+</div>
+<div class="col-sm-6">
+#3f98cf
+</div>
+</div>
+<div class='row chat-msg-Pompona'>
+<div class="col-sm-6">
+Pompona
+</div>
+<div class="col-sm-6">
+#35987a
+</div>
+</div>
+<div class='row chat-msg-Tweedpawn'>
+<div class="col-sm-6">
+Tweedpawn
+</div>
+<div class="col-sm-6">
+#0f98ab
+</div>
+</div>
+<div class='row chat-msg-StarlikeSilences'>
+<div class="col-sm-6">
+StarlikeSilences
+</div>
+<div class="col-sm-6">
+#969cf2
+</div>
+</div>
+<div class='row chat-msg-freeky'>
+<div class="col-sm-6">
+freeky
+</div>
+<div class="col-sm-6">
+#359c46
+</div>
+</div>
+<div class='row chat-msg-Orangensaft'>
+<div class="col-sm-6">
+Orangensaft
+</div>
+<div class="col-sm-6">
+#e79f62
+</div>
+</div>
+<div class='row chat-msg-OrevD'>
+<div class="col-sm-6">
+OrevD
+</div>
+<div class="col-sm-6">
+#6ea0f4
+</div>
+</div>
+<div class='row chat-msg-jacy'>
+<div class="col-sm-6">
+jacy
+</div>
+<div class="col-sm-6">
+#6aa1ff
+</div>
+</div>
+<div class='row chat-msg-wanderingThyme'>
+<div class="col-sm-6">
+wanderingThyme
+</div>
+<div class="col-sm-6">
+#FFA500 
+</div>
+</div>
+<div class='row chat-msg-Jarin'>
+<div class="col-sm-6">
+Jarin
+</div>
+<div class="col-sm-6">
+#e3a6ff
+</div>
+</div>
+<div class='row chat-msg-hex'>
+<div class="col-sm-6">
+hex
+</div>
+<div class="col-sm-6">
+#00a758
+</div>
+</div>
+<div class='row chat-msg-edgke'>
+<div class="col-sm-6">
+edgke
+</div>
+<div class="col-sm-6">
+#f9a82f
+</div>
+</div>
+<div class='row chat-msg-DorianDangerfield'>
+<div class="col-sm-6">
+DorianDangerfield
+</div>
+<div class="col-sm-6">
+#98a9fd
+</div>
+</div>
+<div class='row chat-msg-ardentlyThieving'>
+<div class="col-sm-6">
+ardentlyThieving
+</div>
+<div class="col-sm-6">
+#ffaaff
+</div>
+</div>
+<div class='row chat-msg-tez'>
+<div class="col-sm-6">
+tez
+</div>
+<div class="col-sm-6">
+#FFADAD
+</div>
+</div>
+<div class='row chat-msg-AnotherGayOrc'>
+<div class="col-sm-6">
+AnotherGayOrc
+</div>
+<div class="col-sm-6">
+#6cB066
+</div>
+</div>
+<div class='row chat-msg-pannee'>
+<div class="col-sm-6">
+pannee
+</div>
+<div class="col-sm-6">
+#f9b59e
+</div>
+</div>
+<div class='row chat-msg-lilyvonpseudonym'>
+<div class="col-sm-6">
+lilyvonpseudonym
+</div>
+<div class="col-sm-6">
+#38b6d2
+</div>
+</div>
+<div class='row chat-msg-Sahoni'>
+<div class="col-sm-6">
+Sahoni
+</div>
+<div class="col-sm-6">
+#6ab6d4
+</div>
+</div>
+<div class='row chat-msg-JannockJess'>
+<div class="col-sm-6">
+JannockJess
+</div>
+<div class="col-sm-6">
+#ffb732
+</div>
+</div>
+<div class='row chat-msg-Rusc'>
+<div class="col-sm-6">
+Rusc
+</div>
+<div class="col-sm-6">
+#a6b9bf
+</div>
+</div>
+<div class='row chat-msg-Inay'>
+<div class="col-sm-6">
+Inay
+</div>
+<div class="col-sm-6">
+#2fbf47
+</div>
+</div>
+<div class='row chat-msg-LeeshaJoy'>
+<div class="col-sm-6">
+LeeshaJoy
+</div>
+<div class="col-sm-6">
+#66c358
+</div>
+</div>
+<div class='row chat-msg-masterread'>
+<div class="col-sm-6">
+masterread
+</div>
+<div class="col-sm-6">
+#7fc9ff
+</div>
+</div>
+<div class='row chat-msg-SineWave'>
+<div class="col-sm-6">
+SineWave
+</div>
+<div class="col-sm-6">
+#39c9bb
+</div>
+</div>
+<div class='row chat-msg-BirdMomRaven'>
+<div class="col-sm-6">
+BirdMomRaven
+</div>
+<div class="col-sm-6">
+#1dcbf0
+</div>
+</div>
+<div class='row chat-msg-Siver'>
+<div class="col-sm-6">
+Siver
+</div>
+<div class="col-sm-6">
+#1dcbf0
+</div>
+</div>
+<div class='row chat-msg-foxy114'>
+<div class="col-sm-6">
+foxy114
+</div>
+<div class="col-sm-6">
+#66ccff
+</div>
+</div>
+<div class='row chat-msg-Peagravel'>
+<div class="col-sm-6">
+Peagravel
+</div>
+<div class="col-sm-6">
+#66ccff
+</div>
+</div>
+<div class='row chat-msg-EnbyTheWitch'>
+<div class="col-sm-6">
+EnbyTheWitch
+</div>
+<div class="col-sm-6">
+#ffcc00
+</div>
+</div>
+<div class='row chat-msg-InThroughTheSunroof'>
+<div class="col-sm-6">
+InThroughTheSunroof
+</div>
+<div class="col-sm-6">
+#bfd2c4
+</div>
+</div>
+<div class='row chat-msg-winterpalace'>
+<div class="col-sm-6">
+winterpalace
+</div>
+<div class="col-sm-6">
+#00d2ff
+</div>
+</div>
+<div class='row chat-msg-sugarsugar'>
+<div class="col-sm-6">
+sugarsugar
+</div>
+<div class="col-sm-6">
+#f4d678
+</div>
+</div>
+<div class='row chat-msg-lokiagentofasgard'>
+<div class="col-sm-6">
+lokiagentofasgard
+</div>
+<div class="col-sm-6">
+#f4d678
+</div>
+</div>
+<div class='row chat-msg-tan'>
+<div class="col-sm-6">
+tan
+</div>
+<div class="col-sm-6">
+#F0D7B2
+</div>
+</div>
+<div class='row chat-msg-lanibgoode'>
+<div class="col-sm-6">
+lanibgoode
+</div>
+<div class="col-sm-6">
+#bfd8c2
+</div>
+</div>
+<div class='row chat-msg-ElusiveTumblrDad'>
+<div class="col-sm-6">
+ElusiveTumblrDad
+</div>
+<div class="col-sm-6">
+#add8e6
+</div>
+</div>
+<div class='row chat-msg-CathyFowl'>
+<div class="col-sm-6">
+CathyFowl
+</div>
+<div class="col-sm-6">
+#ADD8E6
+</div>
+</div>
+<div class='row chat-msg-Quirky'>
+<div class="col-sm-6">
+Quirky
+</div>
+<div class="col-sm-6">
+#67d967
+</div>
+</div>
+<div class='row chat-msg-radio-charlie'>
+<div class="col-sm-6">
+radio-charlie
+</div>
+<div class="col-sm-6">
+#b7daff
+</div>
+</div>
+<div class='row chat-msg-bibliomom'>
+<div class="col-sm-6">
+bibliomom
+</div>
+<div class="col-sm-6">
+#bfdbf5
+</div>
+</div>
+<div class='row chat-msg-Omskivar'>
+<div class="col-sm-6">
+Omskivar
+</div>
+<div class="col-sm-6">
+#93dfb3
+</div>
+</div>
+<div class='row chat-msg-Banditkitty'>
+<div class="col-sm-6">
+Banditkitty
+</div>
+<div class="col-sm-6">
+#4ee05f
+</div>
+</div>
+<div class='row chat-msg-HungryHungryHobbit'>
+<div class="col-sm-6">
+HungryHungryHobbit
+</div>
+<div class="col-sm-6">
+#7ae26f
+</div>
+</div>
+<div class='row chat-msg-cmot'>
+<div class="col-sm-6">
+cmot
+</div>
+<div class="col-sm-6">
+#7ae26f
+</div>
+</div>
+<div class='row chat-msg-dreamfluxx'>
+<div class="col-sm-6">
+dreamfluxx
+</div>
+<div class="col-sm-6">
+#a1e5fb
+</div>
+</div>
+<div class='row chat-msg-sugarragus'>
+<div class="col-sm-6">
+sugarragus
+</div>
+<div class="col-sm-6">
+#90e5a2
+</div>
+</div>
+<div class='row chat-msg-Clegga'>
+<div class="col-sm-6">
+Clegga
+</div>
+<div class="col-sm-6">
+#4ee553
+</div>
+</div>
+<div class='row chat-msg-ParallelPenguins'>
+<div class="col-sm-6">
+ParallelPenguins
+</div>
+<div class="col-sm-6">
+#90e5a2
+</div>
+</div>
+<div class='row chat-msg-Grantaire'>
+<div class="col-sm-6">
+Grantaire
+</div>
+<div class="col-sm-6">
+#84ec50
+</div>
+</div>
+<div class='row chat-msg-Alibun'>
+<div class="col-sm-6">
+Alibun
+</div>
+<div class="col-sm-6">
+#8befc5
+</div>
+</div>
+<div class='row chat-msg-onlyasmalllizard'>
+<div class="col-sm-6">
+onlyasmalllizard
+</div>
+<div class="col-sm-6">
+#54f267
+</div>
+</div>
+<div class='row chat-msg-Peri-dot'>
+<div class="col-sm-6">
+Peri-dot
+</div>
+<div class="col-sm-6">
+#83f2bd
+</div>
+</div>
+<div class='row chat-msg-Deaalethiae'>
+<div class="col-sm-6">
+Deaalethiae
+</div>
+<div class="col-sm-6">
+#18f5c6
+</div>
+</div>
+<div class='row chat-msg-Lilai'>
+<div class="col-sm-6">
+Lilai
+</div>
+<div class="col-sm-6">
+#99fd67
+</div>
+</div>
+<div class='row chat-msg-justameme'>
+<div class="col-sm-6">
+justameme
+</div>
+<div class="col-sm-6">
+#feff6e
+</div>
+</div>
+<div class='row chat-msg-diloolie2'>
+<div class="col-sm-6">
+diloolie2
+</div>
+<div class="col-sm-6">
+#c6ffb4
+</div>
+</div>
+<div class='row chat-msg-Elerena'>
+<div class="col-sm-6">
+Elerena
+</div>
+<div class="col-sm-6">
+#7FFFD4
+</div>
+</div>
+<div class='row chat-msg-otteroflore'>
+<div class="col-sm-6">
+otteroflore
+</div>
+<div class="col-sm-6">
+#97ffcd
+</div>
+</div>
+<div class='row chat-msg-MorgynLeri'>
+<div class="col-sm-6">
+MorgynLeri
+</div>
+<div class="col-sm-6">
+#bb3232
+</div>
+</div>
+<div class='row chat-msg-xenzen'>
+<div class="col-sm-6">
+xenzen
+</div>
+<div class="col-sm-6">
+#ef4d3a
+</div>
+</div>
+<div class='row chat-msg-DracoNako'>
+<div class="col-sm-6">
+DracoNako
+</div>
+<div class="col-sm-6">
+#f44d4d
+</div>
+</div>
+<div class='row chat-msg-Curlywench'>
+<div class="col-sm-6">
+Curlywench
+</div>
+<div class="col-sm-6">
+#8450
+</div>
+</div>
+<div class='row chat-msg-ssalogei'>
+<div class="col-sm-6">
+ssalogei
+</div>
+<div class="col-sm-6">
+#fb51e2
+</div>
+</div>
+<div class='row chat-msg-RedLeader'>
+<div class="col-sm-6">
+RedLeader
+</div>
+<div class="col-sm-6">
+#fd5252
+</div>
+</div>
+<div class='row chat-msg-BadTealeaves'>
+<div class="col-sm-6">
+BadTealeaves
+</div>
+<div class="col-sm-6">
+#e35210
+</div>
+</div>
+<div class='row chat-msg-gigitastic'>
+<div class="col-sm-6">
+gigitastic
+</div>
+<div class="col-sm-6">
+#de5cf6
+</div>
+</div>
+<div class='row chat-msg-Scuzz'>
+<div class="col-sm-6">
+Scuzz
+</div>
+<div class="col-sm-6">
+#a85dee
+</div>
+</div>
+<div class='row chat-msg-N3ma1i'>
+<div class="col-sm-6">
+N3ma1i
+</div>
+<div class="col-sm-6">
+#a85dee
+</div>
+</div>
+<div class='row chat-msg-Pandoraylam'>
+<div class="col-sm-6">
+Pandoraylam
+</div>
+<div class="col-sm-6">
+#e55ea2
+</div>
+</div>
+
+
+
+
+
+
+        </div>
+<div class="col-sm-5"></div>
+</div>
